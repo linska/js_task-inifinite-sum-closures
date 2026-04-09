5c237c2dbca05dfea48d3c0de582bd5bae4f2fd3
linska <linskatanya@gmail.com>
2026-04-09 16:48:07 +0300

    feat: implement makeAdder function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,26 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let accumulator = 0;
+  let calledClear = false;
+
+  const infinityAdder = (value) => {
+    if (value === undefined) {
+      if (calledClear) {
+        accumulator = 0;
+      }
+
+      calledClear = true;
+
+      return accumulator;
+    }
+    accumulator += value;
+    calledClear = false;
+
+    return infinityAdder;
+  };
+
+  return infinityAdder;
 }
 
 module.exports = makeInfinityAdder;
